c8b78ae52a52fa9c582dcf270b7dc426f50bc8b8
bennyS007 <kevinjoseph304@gmail.com>
2021-11-25 13:56:47 -0800

    First commit, empty project and .gitignore

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform jumpPoint;
+
+    public float speed;
+
+    public float rotationY = -90f;
+    public float rotationSpeed;
+    public LayerMask groundMask;
+    public bool isGrounded;
+
+
+    Rigidbody rb;
+    float jumpVelocity;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(jumpPoint.position, 0.5f, groundMask) ;
+
+        if(isGrounded && rb.velocity.y < 0)
+        {
+            jumpVelocity = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jumpVelocity = Mathf.Sqrt(3f * -2f * -9.81f);
+        }
+        
+        jumpVelocity += -9.81f * Time.deltaTime;
+        rb.velocity = new Vector3(rb.velocity.x, jumpVelocity, 0f);
+        RotatePlayer();
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y, 0f);
+    }
+
+    void RotatePlayer()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, -Mathf.Abs(rotationY), 0f);
+        } 
+    }
+}
