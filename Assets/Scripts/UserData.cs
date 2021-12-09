2079f0e659d0c08c62654840692ca93ce68676d3
Tej Sidhu <tsidhu@outlook.com>
2021-12-09 10:07:16 -0800

    FIghtScene3 + datagathering scene

---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserData : MonoBehaviour
+{
+
+    public InputField userNameInput;
+    public string userName;
+
+    public string dataCollection()
+    {
+        userName = userNameInput.text;
+        Debug.Log(userName);
+        return userName;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
