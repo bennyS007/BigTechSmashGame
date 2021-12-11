5c628b053eb87d2249d902166704cabbb1c2976b
Sauravgorjilla <68978343+Sauravgorjilla@users.noreply.github.com>
2021-12-11 15:15:55 -0800

    LevelCompleteScreen
    Added levelcompletescreen pls fix error @bennyS007 or else ur bad

---
+++ Assets/Scripts/LevelCompleteScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelCompleteScript : MonoBehaviour
+{
+    public TextMeshProUGUI LevelCompleteText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LevelCompleteText.text = "Congratulations! You beat the level and have successfully protected your SSN: " + UserData.ssn + " by using a VPN!";
+    }
+}
