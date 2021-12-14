ed678eb348c04b3e4f0d04d9e0831b63745b0c99
bennyS007 <Kevinjoseph304@gmail.com>
2021-12-13 19:46:19 -0800

    amogus

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+
 
 public class PlayerStats : MonoBehaviour
 {
-    public static int lives;
-    public static float dmg;
+    public int lives;
+    public float dmg;
+    public Text txt;
 
     Rigidbody rb;
-
+    Vector3 resetPoint;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        resetPoint = this.transform.position + new Vector3(0, 3.5f, 0);
         dmg = 0;
         lives = 3;
     }
@@ -23,6 +25,13 @@ public class PlayerStats : MonoBehaviour
         {
             return;
         }
+
+        if (transform.position.y < -20)
+        {
+            Die();
+        }
+
+        txt.text = dmg.ToString();
     }
 
     public void TakeDamage(int damage)
@@ -34,5 +43,16 @@ public class PlayerStats : MonoBehaviour
     {
         rb.AddForce((this.transform.position - attacker.transform.position) * (dmg/10), ForceMode.Impulse);
     }
-    
+    void Die()
+    {
+        lives--;
+        dmg = 0;
+        Respawn();
+    }
+
+    void Respawn()
+    {
+        transform.position = resetPoint;
+    }
+
 }
